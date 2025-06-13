5d4d8ec30c54f6594dbfeb58e3b4fccc689f0976
luis <salinasgonzalezluisantonio@gmail.com>
2025-06-13 13:43:11 -0600

    Cambios raros de ori

--- nuevos_logros.js
+++ nuevos_logros.js
@@ -7,7 +7,7 @@ window.addEventListener('DOMContentLoaded', () => {
         if (data.logro_nivel_conseguido > 0) {
             const div_logro = document.createElement("div");
             const img_logro = document.createElement("img");
-            img_logro.src = "assets/nivel_desbloqueado.png";
+            img_logro.src = "assets/logro_niveles_desbloqueado.png";
             const texto_logro = document.createElement("p");
             if (data.logro_nivel_conseguido == 1) {
                 texto_logro.textContent = "1 nivel";
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
         if (data.logro_racha_conseguido > 0) {
             const div_logro = document.createElement("div");
             const img_logro = document.createElement("img");
-            img_logro.src = "assets/racha_desbloqueado.png";
+            img_logro.src = "assets/logro_racha_desbloqueado.png";
             const texto_logro = document.createElement("p");
             if (data.logro_racha_conseguido == 1) {
                 texto_logro.textContent = "1 dia de racha";
@@ -38,10 +38,26 @@ window.addEventListener('DOMContentLoaded', () => {
             nuevos_logros = true;
         }
 
+        if (data.logro_tienda_conseguido > 0) {
+            const div_logro = document.createElement("div");
+            const img_logro = document.createElement("img");
+            img_logro.src = "assets/logro_tienda_desbloqueado.png";
+            const texto_logro = document.createElement("p");
+            if (data.logro_tienda_conseguido == 1) {
+                texto_logro.textContent = "25% de tienda";
+            } else {
+                texto_logro.textContent = data.logro_tienda_conseguido + " % de tienda";
+            }
+            div_logro.appendChild(img_logro);
+            div_logro.appendChild(texto_logro);
+            document.getElementById('logros').appendChild(div_logro);
+            nuevos_logros = true;
+        }
+
         if (data.logro_lp_conseguido > 0) {
             const div_logro = document.createElement("div");
             const img_logro = document.createElement("img");
-            img_logro.src = "assets/perfeccion_desbloqueado.png";
+            img_logro.src = "assets/logro_lecciones_perfectas_desbloqueado.png";
             const texto_logro = document.createElement("p");
             texto_logro.textContent = data.logro_lp_conseguido + " lecciones perfectas";
             div_logro.appendChild(img_logro);
@@ -53,7 +69,7 @@ window.addEventListener('DOMContentLoaded', () => {
         if (data.logro_puntaje_conseguido > 0) {
             const div_logro = document.createElement("div");
             const img_logro = document.createElement("img");
-            img_logro.src = "assets/puntaje_desbloqueado.png";
+            img_logro.src = "assets/logro_puntaje_desbloqueado.png";
             const texto_logro = document.createElement("p");
             texto_logro.textContent = data.logro_puntaje_conseguido + " estrellas";
             div_logro.appendChild(img_logro);
